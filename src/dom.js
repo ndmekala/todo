fb5b10fd052af904223769991ec1ceee8baf4873
Nirmal Mekala <ndmekala@icloud.com>
2021-02-23 17:35:21 -0600

    to dos alphabetized by proj, disp w proj headings

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,6 @@
 import { toDo } from './todo.js'
 
 var domLogic = (function () {
-    // make to do
     const add = document.querySelector('#add')
     add.addEventListener(('click'), () => {
         document.querySelector('#my-form').style.display = 'block';
@@ -10,31 +9,76 @@ var domLogic = (function () {
     closebox.addEventListener(('click'), () => {
         document.querySelector('#my-form').style.display = 'none';
     })
+    // “You should separate your application logic (i.e. creating new todos, setting todos
+    // as complete, changing todo priority etc.) from the DOM -related stuff
+    // so keep all of those things in sepearate modules”
+    // Heheh 😅
     const newtodo = document.querySelector('#newtodo')
     newtodo.addEventListener(('click'), () => {
-        // let task = document.querySelector('input[name=task"]');
-        // console.log(task);
-        // var notes = document.querySelector('input[name="notes"]');
-        // var duedate = document.querySelector('input[name="duedate"]');
-        // // var priority = document.querySelector('select[name="priority"]');
-        // var checklist = document.querySelector('input[name="checklist"]');
-        // var project = document.querySelector('input[name="project"]');
-        toDo.add(toDo.make('pink', 'elephants', 'roam', 'in', 'our', 'midst'))
-        // toDo.add(toDo.make(task, task, task, task, task, task, task))
-        // console.table(JSON.parse(localStorage.taskArray));
+        let task = document.getElementById('task').value;
+        let notes = document.getElementById('notes').value;
+        let duedate = document.getElementById('duedate').value;
+        let priority = document.getElementById('priority').value;
+        let checklist = document.getElementById('checklist').value;
+        let project = document.getElementById('project').value;
+        toDo.add(toDo.make(task, notes, duedate, priority, checklist, project));
+        domLogic.clearDOM();
+        domLogic.pagePopulate(domLogic.sortTaskArray(JSON.parse(localStorage.taskArray)));
+        domLogic.buildProjectArray(JSON.parse(localStorage.taskArray))
         document.querySelector('#my-form').style.display = 'none';
     })
     return {
-        pagePopulate: function(a) {
+        clearDOM: function () {
             const box = document.querySelector('#testdiv')
-            // make sub-arrays for each different project with sort or filter or something
+            while (box.firstChild) {
+                box.removeChild(box.firstChild);
+            }
+            box.innerHTML += "<h1>Checklist ✅</h1>"
+        },
+        sortTaskArray: function (a) {
+            const b = a.sort(function(first, next) {
+                return first.project > next.project ? 1 : -1
+            });
+            // okay maybe first sort by date then by project and print??
+            // add date sorting once date format regulated
+            return b
+        },
+        buildProjectArray: function (a) {
+            let b = []
+            a.forEach(element => {
+                if (!b.includes(element.project)) {
+                    b.push(element.project)
+                }
+            })
+            const projectArray = b.sort(function(first, next) {
+                return first > next ? 1 : -1
+            })
+            return projectArray
+        },
+        pagePopulate: function(a) {
+            const box = document.querySelector('#testdiv');
+            // consider how to make this work with *no* project…
             const displayElement = (currentValue) => {
+                // build this without .innerHTML… use appendChild or something
+                // will need this for the nuanced styling each to-do will need…
                 for (let key in currentValue) {
+                    // and may need to do this without “in” because different keys
+                    // have different needs
                     box.innerHTML += key + ': ' + currentValue[key] + '<br>';
                 }
                 box.innerHTML += '<br>'
             }
-            a.forEach(element => displayElement(element))
+            let projectArray = domLogic.buildProjectArray(a);
+            projectArray.forEach(element => {
+                box.innerHTML += `<h2>${element}</h2><br>`
+                const subset = a.filter(e => e.project === element)
+                subset.forEach(todo => displayElement(todo))
+            })
+
+
+
+            // make sub-arrays for each different project with sort or filter or something
+            // a.forEach(element => displayElement(element))
         }
     }
 })();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import { toDo } from './todo.js'
 import { domLogic } from './dom.js'
 
-
-function createProject() {
-
+if (localStorage.taskArray) {
+    console.table(JSON.parse(localStorage.taskArray));
+    domLogic.pagePopulate(JSON.parse(localStorage.taskArray));
 }
-
-
-delete localStorage.taskArray
-toDo.add(toDo.make('Buy coffee.', 'Preferably light roast.', '2/28/21', 'High', ['Pick out coffee.', 'Purchase.'], 'Home'))
-toDo.add(toDo.make('Buy eggs.', 'Cage free.', '2/28/21', 'High', ['Pick out eggs.', 'Purchase.'], 'Home'))
-console.table(JSON.parse(localStorage.taskArray));
-domLogic.pagePopulate(JSON.parse(localStorage.taskArray));
