a1f4208af2e3415f362a83a51adfe97c68933265
Nirmal Mekala <ndmekala@icloud.com>
2021-04-14 18:05:41 -0500

    format again with prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import { toDo } from './todo.js'
-import { domLogic } from './dom.js'
+import { toDo } from "./todo.js";
+import { domLogic } from "./dom.js";
 
 domLogic.addPopupListeners();
 
 if (localStorage.taskArray) {
-    domLogic.pagePopulate(domLogic.sortTaskArray(JSON.parse(localStorage.taskArray)));
+  domLogic.pagePopulate(
+    domLogic.sortTaskArray(JSON.parse(localStorage.taskArray))
+  );
 } else {
-    toDo.add(toDo.make('Open this task by clicking on it.', 'See the notes and controls? Now you can check it off by clicking its bullet.', new Date(), 'Get Oriented'))
-    toDo.add(toDo.make('Edit this todo by clicking on its notes.', 'Make these bugs go away: 🐞🐞🐞. Then save the todo.', new Date(), 'Get Oriented'));
-    toDo.add(toDo.make('Create a new todo with the blue buttons.', 'It’s pretty straightforward!', new Date(), 'Get Oriented'))
-    domLogic.pagePopulate(domLogic.sortTaskArray(JSON.parse(localStorage.taskArray)));
+  toDo.add(
+    toDo.make(
+      "Open this task by clicking on it.",
+      "See the notes and controls? Now you can check it off by clicking its bullet.",
+      new Date(),
+      "Get Oriented"
+    )
+  );
+  toDo.add(
+    toDo.make(
+      "Edit this todo by clicking on its notes.",
+      "Make these bugs go away: 🐞🐞🐞. Then save the todo.",
+      new Date(),
+      "Get Oriented"
+    )
+  );
+  toDo.add(
+    toDo.make(
+      "Create a new todo with the blue buttons.",
+      "It’s pretty straightforward!",
+      new Date(),
+      "Get Oriented"
+    )
+  );
+  domLogic.pagePopulate(
+    domLogic.sortTaskArray(JSON.parse(localStorage.taskArray))
+  );
 }
--- src/todo.js
+++ src/todo.js
@@ -1,40 +1,46 @@
 var toDo = (function () {
-    return {
-        make: function (task, notes, dueDate, project) {
-            return {task, notes, dueDate, project}
-        },
-        add: function(a) {
-            let arr
-            if (localStorage.taskArray) {
-                arr = JSON.parse(localStorage.taskArray)
-            } else {
-                arr = [];
-            }
-            arr.push(a);
-            localStorage.taskArray = JSON.stringify(arr);
-        }, 
-        delete: function(obj) {
-            let arr = JSON.parse(localStorage.taskArray)
-            let pos = arr.findIndex(x =>    x.task === obj.task &&
-                                            x.notes === obj.notes &&
-                                            x.dueDate === obj.dueDate &&
-                                            x.project === obj.project)
-            arr.splice(pos, 1)
-            localStorage.taskArray = JSON.stringify(arr);
-        },
-        edit: function(obj, t, n, d, pro) {
-            let arr = JSON.parse(localStorage.taskArray)
-            let pos = arr.findIndex(x =>    x.task === obj.task &&
-                                            x.notes === obj.notes &&
-                                            x.dueDate === obj.dueDate &&
-                                            x.project === obj.project)
-            arr[pos].task = t
-            arr[pos].notes = n
-            arr[pos].dueDate = d
-            arr[pos].project = pro
-            localStorage.taskArray = JSON.stringify(arr);
-        }
-    }
+  return {
+    make: function (task, notes, dueDate, project) {
+      return { task, notes, dueDate, project };
+    },
+    add: function (a) {
+      let arr;
+      if (localStorage.taskArray) {
+        arr = JSON.parse(localStorage.taskArray);
+      } else {
+        arr = [];
+      }
+      arr.push(a);
+      localStorage.taskArray = JSON.stringify(arr);
+    },
+    delete: function (obj) {
+      let arr = JSON.parse(localStorage.taskArray);
+      let pos = arr.findIndex(
+        (x) =>
+          x.task === obj.task &&
+          x.notes === obj.notes &&
+          x.dueDate === obj.dueDate &&
+          x.project === obj.project
+      );
+      arr.splice(pos, 1);
+      localStorage.taskArray = JSON.stringify(arr);
+    },
+    edit: function (obj, t, n, d, pro) {
+      let arr = JSON.parse(localStorage.taskArray);
+      let pos = arr.findIndex(
+        (x) =>
+          x.task === obj.task &&
+          x.notes === obj.notes &&
+          x.dueDate === obj.dueDate &&
+          x.project === obj.project
+      );
+      arr[pos].task = t;
+      arr[pos].notes = n;
+      arr[pos].dueDate = d;
+      arr[pos].project = pro;
+      localStorage.taskArray = JSON.stringify(arr);
+    },
+  };
 })();
 
-export { toDo }
+export { toDo };
